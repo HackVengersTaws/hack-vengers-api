b29adf23017382cc250ce6455dbe818cdd98c7b6
Josue Davalos <jedavalo@espol.edu.ec>
2020-09-30 20:36:28 -0500

    Create Filter of tweets

--- api_twitter/tweet.py
+++ api_twitter/tweet.py
@@ -16,18 +16,11 @@ auth.set_access_token(access_token , access_token_secret)
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
 
-ids =  ['1212950212546879490','1309047475420921857']
-list_of_tw_status = api.statuses_lookup(ids, tweet_mode= "extended")
-
 def get_data(data): 
     return json.dumps(data._json, indent=2, ensure_ascii=False)
 
-def get_data_J(data):
+def get_data_Json(data):
     return data._json
-
-"""for tweet in list_of_tw_status:
-    print(get_data(tweet))
-    print('\n\n\n')"""
 
 def menciones(list):
     menciones = []
@@ -38,6 +31,11 @@ def menciones(list):
 
 
 
+def get_hashtags(list_dic):
+    l = []
+    for dic_hashtag in list_dic:
+        l.append(dic_hashtag['text'])
+    return l
 
 def diccionario_tw (tw):
     dic = {}
@@ -45,22 +43,89 @@ def diccionario_tw (tw):
     dic["texto"] = tw['full_text']
     dic["longuitud"] = len( dic["texto"])
     dic_entity = tw['entities']
-    list_hashtags = dic_entity['hashtags']
+    list_hashtags = get_hashtags(dic_entity['hashtags'])
     dic["hashtags"] = list_hashtags
     dic["num_hashtags"] = len(list_hashtags)
     dic["mencions"] = menciones(dic_entity['user_mentions'])
     dic["num_mencions"] = len(menciones(dic_entity['user_mentions']))
+    dic['language'] = tw['lang']
+    dic['retweet_count'] = tw['retweet_count']
+    dic['favorite_count'] = tw['favorite_count']
+    
+    dic_place = tw['place']
+    if dic_place is not None:
+        dic['name_place'] = dic_place['name']
+        dic['full_name_place'] = dic_place['full_name']
+        dic['country'] = dic_place['country']
+    else:
+        dic['name_place'] = np.nan
+        dic['full_name_place'] = np.nan
+        dic['country'] = np.nan
+
+
+
     dic_user = tw['user']
     dic["username"] = dic_user['screen_name']
-    dic["country"] = dic_user['location']
     dic["followers"] = dic_user['followers_count']
     dic["friends"] = dic_user['friends_count']
-    dic["fech_creacion"] = tw['created_at']
+    dic["fecha_count"] = dic_user['created_at']
+
     return dic
 
-lista_tweets = []
-for tweet in list_of_tw_status:
-    tw = get_data_J(tweet)
-    dicc = diccionario_tw (tw)
-    lista_tweets.append(dicc)
-print(lista_tweets)
+
+
+def get_info_tweets(list_of_tweet):
+    lista_tweets = []
+    for tweet in list_of_tweet:
+        tw = get_data_Json(tweet)
+        dicc = diccionario_tw (tw)
+        lista_tweets.append(dicc)
+    return lista_tweets
+
+
+
+
+def get_tweets_from_tweepy(keywords, hashtags=[], mencions=[], since_date=None, end_date=None, country=None,
+                          min_replies=None, min_faves=None, min_retweets=None, from_count=None, language=None):
+    filters = ''
+
+    if country is not None:
+        country_id = api.geo_search(query=country, granularity="country")[0].id
+        filters += 'place:%s ' %country_id
+    if min_replies is not None:
+        filters += 'min_replies:%d ' %min_replies
+    if min_faves is not None:
+        filters += 'min_faves:%d ' %min_faves
+    if min_retweets is not None:
+        filters += 'min_retweets:%d ' %min_retweets
+    if from_count is not None:
+        filters += 'from:%s ' %from_count
+
+
+    hashtags = ['#'+x for x in hashtags]
+    mencions = ['@'+x for x in mencions]
+    keywords = ' OR '.join(keywords+hashtags+mencions)
+
+    new_tweets = tweepy.Cursor(api.search, 
+                               q='(%s) %s' % (keywords, filters), 
+                               since = since_date,
+                               until = end_date,
+                               tweet_mode='extended',
+                               include_entities=True,
+                               count=100,
+                               lang=language,
+                               ).items(1)
+
+    return list(new_tweets)
+
+
+def get_DataFrame(tweets):
+    df = pd.DataFrame(tweets)
+    df['fecha'] = pd.to_datetime(df['fecha'])
+    df['fecha_count'] = pd.to_datetime(df['fecha_count'])
+    return df
+
+
+# tweets_apis = get_tweets_from_tweepy(['a'], country='Ecuador')
+# tweets = get_info_tweets(tweets_apis)
+# print(tweets)
